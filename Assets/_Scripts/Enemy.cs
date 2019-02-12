f4adf4e88f8cccd9a54aaf6860df6f1be74f948d
EtowTheOffieCat <mabonp@gmail.com>
2019-02-12 14:36:12 +0100

    Update 2 12.02.2019

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -1,13 +1,37 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 public class Enemy : MonoBehaviour
-{ 
+{
+    [SerializeField] public class  EnemyEvent : UnityEvent<Enemy>
+    {
+
+    }
+    public EnemyEvent EnemyDied = new EnemyEvent();
     public void SetDestiation(Vector3 goalPos)
     {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.SetDestination(goalPos);
     }
 
-    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+    private void OnDisable()
+    {
+        EnemyDied?.Invoke(this);
+        
+    }
+    private void OnDestroy()
+    {
+        EnemyDied?.Invoke(this);
+    }
+
+
 
 }
---
+++ Assets/_Scripts/Tower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] private List<Enemy> enemies = new List<Enemy>(8);
+    private void OnTriggerEnter(Collider other)
+    {
+        Enemy enemy = other.GetComponent<Enemy>();
+        if (enemy == null) { return; }
+        enemy.EnemyDied.AddListener(OnEnemyDied);
+        enemies.Add(enemy);
+    }
+    private void OnTriggerExit(Collider other)
+    { 
+        Enemy enemy = other.GetComponent<Enemy>();
+        if (enemy == null) { return; }
+        enemy.EnemyDied.RemoveListener(OnEnemyDied);
+        enemies.Remove(enemy);
+    }
+    public void OnEnemyDied(Enemy enemy)
+    {
+        print("Enemy died");
+        enemies.Remove(enemy);
+        enemy.EnemyDied.RemoveListener(OnEnemyDied);
+    }
+}
